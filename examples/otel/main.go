da87f6a9d215bbd7ada8580b7343ca94c640e71b
Jack Kleeman <jackkleeman@gmail.com>
2024-09-17 08:55:14 +0100

    Parse otel headers automatically + create otel example (#39)

---
+++ examples/otel/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	restate "github.com/restatedev/sdk-go"
+	"github.com/restatedev/sdk-go/server"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/sdk/resource"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+)
+
+type Greeter struct{}
+
+func (Greeter) Greet(ctx restate.Context, message string) (string, error) {
+	_, span := otel.Tracer("").Start(ctx, "Greet")
+	defer span.End()
+
+	return fmt.Sprintf("%s!", message), nil
+}
+
+func main() {
+	exporter, err := otlptrace.New(
+		context.Background(),
+		otlptracegrpc.NewClient(
+			otlptracegrpc.WithInsecure(),
+			otlptracegrpc.WithEndpoint("localhost:4317"),
+		),
+	)
+
+	if err != nil {
+		log.Fatalf("Could not set exporter: %v", err)
+	}
+
+	resources, err := resource.New(
+		context.Background(),
+		resource.WithAttributes(
+			attribute.String("service.name", "restate-sdk-go-otel-example-greeter"),
+		),
+	)
+	if err != nil {
+		log.Fatalf("Could not set resources: %v", err)
+	}
+
+	otel.SetTracerProvider(
+		sdktrace.NewTracerProvider(
+			sdktrace.WithSampler(sdktrace.ParentBased(sdktrace.AlwaysSample())),
+			sdktrace.WithSpanProcessor(sdktrace.NewBatchSpanProcessor(exporter)),
+			sdktrace.WithResource(resources),
+		),
+	)
+
+	otel.SetTextMapPropagator(propagation.TraceContext{})
+
+	if err := server.NewRestate().
+		Bind(restate.Reflect(Greeter{})).
+		Start(context.Background(), ":9080"); err != nil {
+		log.Fatal(err)
+	}
+}
--- go.mod
+++ go.mod
@@ -9,14 +9,19 @@ require (
 	github.com/google/uuid v1.6.0
 	github.com/mr-tron/base58 v1.2.0
 	github.com/stretchr/testify v1.9.0
+	go.opentelemetry.io/otel v1.28.0
 	golang.org/x/net v0.23.0
+	golang.org/x/sync v0.8.0
 	google.golang.org/protobuf v1.33.0
 )
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/go-logr/logr v1.4.2 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	golang.org/x/sync v0.8.0 // indirect
+	go.opentelemetry.io/otel/metric v1.28.0 // indirect
+	go.opentelemetry.io/otel/trace v1.28.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
