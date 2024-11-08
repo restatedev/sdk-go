8e020ac9bb3979dc5bb49cd00bde96a4457f6756
Jack Kleeman <jackkleeman@gmail.com>
2024-11-08 11:02:27 +0000

    Update test suite

--- test-services/testutils.go
+++ test-services/testutils.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"os"
 	"strings"
 	"sync/atomic"
 	"time"
@@ -21,6 +22,17 @@ type CreateAwakeableAndAwaitItResponse struct {
 	Value *string `json:"value,omitempty"`
 }
 
+type InterpretRequest struct {
+	ListName string    `json:"listName"`
+	Commands []Command `json:"commands"`
+}
+
+type Command struct {
+	Type         string `json:"type"`
+	AwakeableKey string `json:"awakeableKey"`
+	EnvName      string `json:"envName"`
+}
+
 func init() {
 	REGISTRY.AddDefinition(
 		restate.NewService("TestUtilsService").
@@ -36,6 +48,10 @@ func init() {
 				func(ctx restate.Context, _ restate.Void) (map[string]string, error) {
 					return ctx.Request().Headers, nil
 				})).
+			Handler("rawEcho", restate.NewServiceHandler(
+				func(ctx restate.Context, input []byte) ([]byte, error) {
+					return input, nil
+				}, restate.WithBinary)).
 			Handler("createAwakeableAndAwaitIt", restate.NewServiceHandler(
 				func(ctx restate.Context, req CreateAwakeableAndAwaitItRequest) (CreateAwakeableAndAwaitItResponse, error) {
 					awakeable := restate.Awakeable[string](ctx)
@@ -95,5 +111,46 @@ func init() {
 						})
 					}
 					return invokedSideEffects.Load(), nil
-				})))
+				})).
+			Handler("getEnvVariable", restate.NewServiceHandler(
+				func(ctx restate.Context, env string) (string, error) {
+					return restate.Run(ctx, func(ctx restate.RunContext) (string, error) {
+						return os.Getenv(env), nil
+					})
+				})).
+			Handler("interpretCommands", restate.NewServiceHandler(
+				func(ctx restate.Context, req InterpretRequest) (restate.Void, error) {
+					for _, command := range req.Commands {
+						switch command.Type {
+						case "createAwakeableAndAwaitIt":
+							result, err := createAwakeableAndAwaitIt(ctx, command.AwakeableKey)
+							if err != nil {
+								return restate.Void{}, err
+							}
+							restate.ObjectSend(ctx, "ListObject", req.ListName, "append").Send(result)
+						case "getEnvVariable":
+							result, err := getEnvVariable(ctx, command.EnvName)
+							if err != nil {
+								return restate.Void{}, err
+							}
+							restate.ObjectSend(ctx, "ListObject", req.ListName, "append").Send(result)
+						}
+					}
+					return restate.Void{}, nil
+				})),
+	)
+}
+
+func createAwakeableAndAwaitIt(ctx restate.Context, awakeableKey string) (string, error) {
+	awakeable := restate.Awakeable[string](ctx)
+	if _, err := restate.Object[restate.Void](ctx, "AwakeableHolder", awakeableKey, "hold").Request(awakeable.Id()); err != nil {
+		return "", err
+	}
+	return awakeable.Result()
+}
+
+func getEnvVariable(ctx restate.Context, envName string) (string, error) {
+	return restate.Run(ctx, func(ctx restate.RunContext) (string, error) {
+		return os.Getenv(envName), nil
+	})
 }
