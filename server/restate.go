40299bd50b0174660260ea6d6c3bbc1d1ca401a5
Francesco Guardiani <francescoguard@gmail.com>
2025-10-20 15:42:42 +0200

    Use the correct logger (#115)

--- server/restate.go
+++ server/restate.go
@@ -696,7 +696,7 @@ func (r *Restate) Start(ctx context.Context, address string) error {
 		return fmt.Errorf("failed to listen on address %s: %w", address, err)
 	}
 
-	slog.Info(fmt.Sprintf("Restate SDK started listening on %s", listener.Addr()))
+	r.systemLog.Info(fmt.Sprintf("Restate SDK started listening on %s", listener.Addr()))
 
 	if err := server.Serve(listener); err != nil && !errors.Is(err, http.ErrServerClosed) {
 		return fmt.Errorf("server error: %w", err)
