1218cbd01042cf1e52f3994f8bd9157e36c94dd8
slinkydeveloper <me@slinkydeveloper.com>
2026-07-13 13:04:32 +0200

    Fix waiting on already completed futures.

--- internal/restatecontext/wait_iterator.go
+++ internal/restatecontext/wait_iterator.go
@@ -19,34 +19,37 @@ type WaitIterator interface {
 }
 
 func (restateCtx *ctx) WaitIter(futs ...Future) WaitIterator {
-	indexedFuts := make(map[uint32]Future, len(futs))
-	for i := range futs {
-		handle := futs[i].handle()
-		indexedFuts[handle] = futs[i]
-	}
+	// Copy for safety.
+	remaining := make([]Future, len(futs))
+	copy(remaining, futs)
 
 	return &waitIterator{
-		restateCtx:  restateCtx,
-		indexedFuts: indexedFuts,
+		restateCtx: restateCtx,
+		remaining:  remaining,
 	}
 }
 
 type waitIterator struct {
 	restateCtx    *ctx
-	indexedFuts   map[uint32]Future
+	remaining     []Future
 	lastCompleted Future
 	cancelled     bool
 }
 
 func (s *waitIterator) Next() bool {
-	if s.cancelled || len(s.indexedFuts) == 0 {
+	if s.cancelled || len(s.remaining) == 0 {
 		s.lastCompleted = nil
 		return false
 	}
 
-	remainingHandles := make([]uint32, 0, len(s.indexedFuts))
-	for k := range s.indexedFuts {
-		remainingHandles = append(remainingHandles, k)
+	remainingHandles := make([]uint32, len(s.remaining))
+	for i, fut := range s.remaining {
+		// If a future is already resolved, return without calling state machine
+		if fut.isResolved() {
+			s.lastCompleted = s.removeAt(i)
+			return true
+		}
+		remainingHandles[i] = fut.handle()
 	}
 
 	// Do progress
@@ -57,21 +60,27 @@ func (s *waitIterator) Next() bool {
 		return false
 	}
 
-	// If we exit, one of them is completed, gotta figure out which one
-	for _, handle := range remainingHandles {
+	// One of the handles is now completed on the state machine side, gotta
+	// figure out which one.
+	for i, handle := range remainingHandles {
 		completed, err := s.restateCtx.stateMachine.IsCompleted(s.restateCtx, handle)
 		if err != nil {
 			panic(err)
 		}
 		if completed {
-			fut := s.indexedFuts[handle]
-			delete(s.indexedFuts, handle)
-			s.lastCompleted = fut
+			s.lastCompleted = s.removeAt(i)
 			return true
 		}
 	}
 
 	panic("Unexpectedly none of the remaining handles completed, this looks like a bug")
+}
+
+// removeAt removes and returns the future at index i.
+func (s *waitIterator) removeAt(i int) Future {
+	fut := s.remaining[i]
+	s.remaining = append(s.remaining[:i], s.remaining[i+1:]...)
+	return fut
 }
 
 func (s *waitIterator) Err() errors.TerminalError {
